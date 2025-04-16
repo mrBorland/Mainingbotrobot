c3c5d96b0eef546bf98f9fe451dd9d151e4112f7
mrBorland <taras-tiva@ukr.net>
2025-04-16 16:16:02 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,40 +1,16 @@
 from aiogram import Bot, Dispatcher, types, executor
-from aiogram.dispatcher.filters import Command
 
-# Твій токен і chat_id
-API_TOKEN = "7567566641:AAGKaV2Qx5GrhXx_a2Ju_h7KrlvJIRVRX1M8"
-OWNER_ID = 6821675571  # Taras
-
-bot = Bot(token=API_TOKEN)
+# Новий токен бота
+bot = Bot(token="7679171745:AAG2ElvAtIWTOG7WQuj7jQWTfQBXx0EUwKI")
 dp = Dispatcher(bot)
+
+# Твій Telegram ID
+YOUR_TELEGRAM_ID = 6821675571
 
 @dp.message_handler(commands=["start"])
 async def start_handler(message: types.Message):
-    if message.from_user.id == OWNER_ID:
+    if message.chat.id == YOUR_TELEGRAM_ID:
         await message.reply("Привіт, Тоні! Бот активний.")
-    else:
-        await message.reply("Доступ заборонено.")
-
-@dp.message_handler(commands=["stats"])
-async def stats_handler(message: types.Message):
-    if message.from_user.id == OWNER_ID:
-        await message.reply("Статистика: 250 акаунтів, 5 платформ, USDT прибуток за сьогодні: $42.60")
-    else:
-        await message.reply("Доступ заборонено.")
-
-@dp.message_handler(commands=["farm"])
-async def farm_handler(message: types.Message):
-    if message.from_user.id == OWNER_ID:
-        await message.reply("Фарм запущено. Перевіряю акаунти...")
-        # Тут буде реальна логіка фарму
-    else:
-        await message.reply("Доступ заборонено.")
-
-@dp.message_handler(commands=["withdraw"])
-async def withdraw_handler(message: types.Message):
-    if message.from_user.id == OWNER_ID:
-        await message.reply("Вивід коштів розпочато...")
-        # Тут буде логіка виводу
     else:
         await message.reply("Доступ заборонено.")
 
